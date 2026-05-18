62162769e8f32f3195e58059fca8561185a42985
Super User <root@Zephyrus.localdomain>
2026-05-17 22:32:00 -0400

    vice versa

---
+++ .built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_busybox_dnf/built-in.o :=  rm -f busybox_dnf/built-in.o; ar rcs busybox_dnf/built-in.o
---
+++ .lib.a.cmd
@@ -0,0 +1,1 @@
+cmd_busybox_dnf/lib.a := rm -f busybox_dnf/lib.a; ar  rcs busybox_dnf/lib.a busybox_dnf/dnf.o
